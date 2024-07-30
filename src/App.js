29b3e705f5df64e9280a4064e82d9b9ab2824391
FURI <63885192+furidngrt@users.noreply.github.com>
2024-07-30 18:51:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './styles/App.css';
+import TrendingVideos from './TrendingVideos';
+import VideoDetail from './VideoDetail';
+import About from './components/navbar/About';
+import Contact from './components/navbar/Contact';
+import Privacy from './components/navbar/Privacy';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/navbar/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<TrendingVideos />} />
+            <Route path="/video/:id" element={<VideoDetail />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy" element={<Privacy />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
